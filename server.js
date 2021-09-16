c6fba7db1d244e0f91242719c00b77072aec4ca2
kaleem <ks@softwarealliance.dk>
2021-09-16 05:15:01 +0500

    mongoose and morgan added and Post model is created

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser= require('body-parser');
+const morgan= require('morgan');
+const dotenv = require('dotenv');
 const app = express();
 
 // Static Files (we dont need to write public word in the links)
@@ -12,13 +14,21 @@ app.set('layout', './layouts/master');       //setting master as default
 app.set('view engine','ejs');
 
 //set listener
-const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT;
 app.listen(port, () => { console.log(`App running on port ${port}`) });
 
+//DB connection
+require('./config/db');
+
+//morgan (it is middleware that saves request logs. For development mode. see on console it will show http requests)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 // This will parse form data (middleware)
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // import the routes
 const generalRoutes = require('./routes/generalRoutes');
